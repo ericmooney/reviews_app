68f01eae9dc2b3e5aa122c3e3b7bf85e551f9701
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-26 22:50:53 -0700

    Added controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,71 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @reviews }
+    end
   end
 
-  def new
+def new
+    @review = Review.new
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @review }
+    end
   end
 
   def create
+    @review = Review.new(params[:review])
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.json { render json: @review, status: :created, location: @review }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+    @review = Review.find(params[:id])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def update
+    @review = Review.find(params[:id])
+
+    respond_to do |format|
+      if @review.update_attributes(params[:review])
+        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
 end
+
